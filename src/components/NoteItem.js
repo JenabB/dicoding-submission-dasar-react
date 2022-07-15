8330afbc7eca434dfb6cf667084d17ba43620f3b
JenabB <Ceasar.Surya@buildbuddy.net.au>
2022-07-15 20:21:52 +0700

    fix note item

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,39 +1,65 @@
-import React, { useState } from "react";
+import React from "react";
 import { showFormattedDate } from "../utils";
 import DeleteModal from "./DeleteModal";
 
-function NoteItem({ id, title, body, onArchive, createdAt, onDelete }) {
-  const [open, setOpen] = useState(false);
+class NoteItem extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const handleDeleteClick = () => setOpen(!open);
+    this.state = {
+      open: false,
+      id: props.id,
+      title: props.title,
+      body: props.body,
+      createdAt: props.createdAt,
+    };
 
-  return (
-    <>
-      {open && (
-        <DeleteModal id={id} onConfirm={onDelete} onClose={handleDeleteClick} />
-      )}
-      <div className="note-item">
-        <h1 className="note-item__title">{title}</h1>
-        <p className="note-item__content">{body}</p>
-        <p className="note-item__date">{showFormattedDate(createdAt)}</p>
-        <div className="note-item__action">
-          <button className="note-item__archive-button">Edit</button>
-          <button
-            className="note-item__archive-button"
-            onClick={() => onArchive(id)}
-          >
-            Archive
-          </button>
-          <button
-            className="note-item__delete-button"
-            onClick={handleDeleteClick}
-          >
-            Delete
-          </button>
+    this.onArchive = props.onArchive;
+    this.onDelete = props.onDelete;
+    this.onDeleteOpen = this.onDeleteOpen.bind(this);
+  }
+
+  onDeleteOpen() {
+    this.setState({
+      open: !this.state.open,
+    });
+  }
+
+  render() {
+    return (
+      <>
+        {this.state.open && (
+          <DeleteModal
+            id={this.state.id}
+            onConfirm={this.onDelete}
+            onClose={this.onDeleteOpen}
+          />
+        )}
+        <div className="note-item">
+          <h1 className="note-item__title">{this.state.title}</h1>
+          <p className="note-item__content">{this.state.body}</p>
+          <p className="note-item__date">
+            {showFormattedDate(this.state.createdAt)}
+          </p>
+          <div className="note-item__action">
+            <button className="note-item__archive-button">Edit</button>
+            <button
+              className="note-item__archive-button"
+              onClick={() => this.onArchive(this.state.id)}
+            >
+              Archive
+            </button>
+            <button
+              className="note-item__delete-button"
+              onClick={this.onDeleteOpen}
+            >
+              Delete
+            </button>
+          </div>
         </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  }
 }
 
 export default NoteItem;
